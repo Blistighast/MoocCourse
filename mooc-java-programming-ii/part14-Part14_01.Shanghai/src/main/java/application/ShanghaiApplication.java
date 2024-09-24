85e5285815a84b34c9f402078d3f83ac39341c4c
Brian Guterl <brianguterl99@gmail.com>
2024-09-23 23:34:17 -0400

    started part 14

--- mooc-java-programming-ii/part14-Part14_01.Shanghai/src/main/java/application/ShanghaiApplication.java
+++ mooc-java-programming-ii/part14-Part14_01.Shanghai/src/main/java/application/ShanghaiApplication.java
@@ -1,9 +1,65 @@
 package application;
 
-public class ShanghaiApplication {
+import java.util.HashMap;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class ShanghaiApplication extends Application {
+
+    @Override
+    public void start(Stage window) {
+        //format data
+        String list = "2007 73\n"
+                + "2008 68\n"
+                + "2009 72\n"
+                + "2010 72\n"
+                + "2011 74\n"
+                + "2012 73\n"
+                + "2013 76\n"
+                + "2014 73\n"
+                + "2015 67\n"
+                + "2016 56\n"
+                + "2017 56";
+        
+        HashMap<Integer, Integer> data = new HashMap<>();
+        
+        String[] listParts = list.split("\n");
+        for (String part: listParts) {
+            String[] dataParts = part.split(" ");
+            data.put(Integer.parseInt(dataParts[0]),Integer.parseInt(dataParts[1]));
+        }
+        
+        //create chart
+        NumberAxis xAxis = new NumberAxis(2006, 2018, 2);
+        NumberAxis yAxis = new NumberAxis(0, 100, 10);
+
+        xAxis.setLabel("Year");
+        yAxis.setLabel("Ranking");
+
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("University of Helsinki, Shanghai ranking");
+        
+        XYChart.Series chartData = new XYChart.Series();
+        
+        //add data to chart
+        data.entrySet().stream().forEach(pair -> {
+            chartData.getData().add(new XYChart.Data(pair.getKey(), pair.getValue()));
+        });
+        lineChart.getData().add(chartData);
+        
+        //display chart
+        Scene view = new Scene(lineChart, 640, 480);
+        window.setScene(view);
+        window.show();
+        
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ShanghaiApplication.class);
     }
 
 }
--- mooc-java-programming-ii/part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
+++ mooc-java-programming-ii/part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
@@ -1,9 +1,84 @@
 package application;
 
-public class PartiesApplication {
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class PartiesApplication extends Application {
+
+    @Override
+    public void start(Stage window) {
+        //get data
+        Map<String, Map<Integer, Double>> values = readFile();
+
+        //create chart
+        NumberAxis xAxis = new NumberAxis(1968, 2008, 4);
+        NumberAxis yAxis = new NumberAxis(0, 30, 5);
+
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Relative support of the parties");
+
+        //add data
+        values.keySet().stream().forEach(party -> {
+            XYChart.Series data = new XYChart.Series();
+            data.setName(party);
+
+            values.get(party).entrySet().stream().forEach(pair -> {
+                data.getData().add(new XYChart.Data(pair.getKey(), pair.getValue()));
+            });
+
+            lineChart.getData().add(data);
+        });
+
+        //display chart
+        Scene view = new Scene(lineChart, 640, 480);
+        window.setScene(view);
+        window.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(PartiesApplication.class);
+    }
+
+    public Map<String, Map<Integer, Double>> readFile() {
+        ArrayList<String> lines = new ArrayList<>();
+        Map<String, Map<Integer, Double>> values = new HashMap<>();
+
+        try (Scanner scanner = new Scanner(Paths.get("partiesdata.tsv"))) {
+            while (scanner.hasNextLine()) {
+                lines.add(scanner.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
+
+        String[] years = lines.get(0).split("\t");
+        lines.remove(0);
+
+        for (String line : lines) {
+            Map<Integer, Double> partyData = new HashMap<>();
+            String[] parts = line.split("\t");
+            for (int i = 1; i < years.length; i++) {
+                if (parts[i].equals("-")) {
+                    continue;
+                }
+
+                partyData.put(Integer.valueOf(years[i]), Double.valueOf(parts[i]));
+            }
+
+            values.put(parts[0], partyData);
+        }
+
+        return values;
     }
 
 }
